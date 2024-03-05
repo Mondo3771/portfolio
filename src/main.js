a4afe15209ad13763cfdbd37921b6937b8572195
Mondo3771 <kabelomrn@gmail.com>
2024-03-05 20:28:43 +0200

    Finished the Hero section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import '../styles/moder-normalise.css';
 import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+
 import '../styles/utils.css';
